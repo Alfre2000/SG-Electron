da31ff803e36c2c9340515608c401bac2bf25510
Alfre200 <alfredo.mauri.2000@gmail.com>
2022-09-24 17:28:50 +0200

    bug fix yml

---
+++ src/pages/GestioneImpianto/RecordLavorazioneOssido/__tests__/RecordLavorazioneOssido.test.js
@@ -0,0 +1,25 @@
+// import { render, screen } from "@testing-library/react";
+// import FormContext from "../../../../contexts/FormContext";
+// import UserContext from "../../../../UserContext";
+// import RecordLavorazioneOssido from "../RecordLavorazioneOssido";
+
+// describe("RecordLavorazioneOssido", () => {
+//   test("Alert render correttamente", async () => {
+//     const formContext = {
+//       view: false,
+//       errors: undefined,
+//       initialData: undefined,
+//     };
+//     const userNormal = {
+//       user: { user: { username: "test2", is_staff: false, impianto: 1 } },
+//       setUser: jest.fn(),
+//     };
+//     render(
+//       <UserContext.Provider value={userNormal}>
+//         <FormContext {...formContext}>
+//           <RecordLavorazioneOssido />
+//         </FormContext>
+//       </UserContext.Provider>
+//     );
+//   });
+// });
