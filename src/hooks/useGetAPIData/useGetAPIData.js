742ce6dd493d617dec9956bfb19cabcb64e8554b
Alfre200 <alfredo.mauri.2000@gmail.com>
2022-11-06 20:59:45 +0100

    Update every 10 minutes

--- src/hooks/useGetAPIData/useGetAPIData.js
+++ src/hooks/useGetAPIData/useGetAPIData.js
@@ -3,7 +3,7 @@ import { apiGet } from "../../api/api";
 import UserContext from "../../UserContext";
 
 // [ { nome, url, parser }, ... ]
-function useGetAPIData(requests, neeedsImpianto) {
+function useGetAPIData(requests, needsImpianto) {
   const { user } = useContext(UserContext);
   const impianto = user?.user?.impianto;
   const [data, setData] = useState({});
@@ -18,15 +18,27 @@ function useGetAPIData(requests, neeedsImpianto) {
       setData((previousState) => ({ ...previousState, ...newData }));
     }
   }, []);
-  useEffect(() => {
-    if (neeedsImpianto && !impianto) return;
+
+  const update = useCallback(() => {
+    if (needsImpianto && !impianto) return;
     requests.forEach((req) => {
       const url = new URL(req.url);
       if (impianto?.id) url.searchParams.append("impianto", impianto.id);
       if (req.nome === "records") url.searchParams.append("page", 1);
       apiGet(url.toString()).then((response) => setNewData(req, response));
     });
-  }, [setNewData, impianto, neeedsImpianto]);
+    console.log("Updating...");
+  }, [impianto, needsImpianto, setNewData]);
+
+  useEffect(() => {
+    update();
+  }, [setNewData, impianto, needsImpianto]);
+
+  useEffect(() => {
+    setInterval(() => {
+      update();
+    }, 1000 * 60 * 10); // Update every 10 minutes
+  }, []);
   return [data, setData];
 }
 
