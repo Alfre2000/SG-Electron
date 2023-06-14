59be80df923dac81ddf53d1a8f897d992b1ead60
Alfredo <alfredo.mauri.2000@gmail.com>
2023-06-14 15:32:59 +0200

    bug fix tests

--- src/programmi.js
+++ src/programmi.js
@@ -10,6 +10,7 @@ export const PROGRAMMI = {
     "Gestione Impianto": {name: 'Gestione Impianto', link: '/manutenzione/selezione-impianto/', icon: ToolBox},
     "Gestione Inventario": {name: 'Gestione Inventario', link: '#', icon: Inventory},
     "Gestione Preventivi": {name: 'Gestione Preventivi', link: '#', icon: Budget},
+    "Programma 4": {name: 'Programma 4', link: '#', icon: WorkInProgress},
     "Programma 5": {name: 'Programma 5', link: '#', icon: WorkInProgress},
     "Programma 6": {name: 'Programma 6', link: '#', icon: WorkInProgress},
     "Area Admin": {name: 'Area Admin', link: '/area-admin/scheda-controllo/', icon: Admin},
