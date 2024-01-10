73fa6a63cadc30a46b0792c618186e8077180318
Alfredo <alfredo.mauri.2000@gmail.com>
2024-01-10 22:50:39 +0100

    Download all certificati button

--- src/pages/CertificatiQualità/DatabaseCertificati/DatabaseCertificati.js
+++ src/pages/CertificatiQualità/DatabaseCertificati/DatabaseCertificati.js
@@ -1,4 +1,4 @@
-import { Card, Container, Placeholder, Table } from "react-bootstrap";
+import { Button, Card, Container, Placeholder, Table } from "react-bootstrap";
 import PageTitle from "../../../components/PageTitle/PageTitle";
 import Wrapper from "../Wrapper";
 import SearchSelect from "../../../components/form-components/SearchSelect";
@@ -12,7 +12,8 @@ import RecordLavorazioneForm from "../../GestioneImpianto/RecordLavorazione/Reco
 import { useState } from "react";
 import Paginator from "../../../components/Pagination/Paginator";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import { faDownload, faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import JSZip from "jszip";
 const electron = window?.require ? window.require("electron") : null;
 
 const dateOptions = {
@@ -22,7 +23,7 @@ const dateOptions = {
 };
 
 function DatabaseCertificati() {
-  const [cliente, setCliente] = useState(null)
+  const [cliente, setCliente] = useState(null);
   const clientiQuery = useQuery([URLS.CLIENTI]);
   const articoliQuery = useQuery([URLS.ARTICOLI_NAMES]);
   const anni = Array.from({ length: new Date().getFullYear() - 2022 }, (_, i) => 2023 + i);
@@ -37,7 +38,7 @@ function DatabaseCertificati() {
       postURL={URLS.RECORD_CERTIFICATI}
       FormComponent={RecordLavorazioneForm}
       impiantoFilter={false}
-      defaultFilters={[...flatFilters, { custom_page_size: 10 }]}
+      defaultFilters={[...flatFilters]}
     >
       <Wrapper>
         <Container className="text-center my-10 lg:mx-2 xl:mx-6 2xl:mx-12">
@@ -46,44 +47,52 @@ function DatabaseCertificati() {
             <Card.Header as="h6" className="font-semibold text-lg">
               Ricerca
             </Card.Header>
-            <Card.Body className="px-5 grid grid-cols-2 gap-y-3 mt-3 mb-1 text-right">
-              <SearchSelect
-                label="Cliente"
-                options={searchOptions(clientiQuery.data, "nome")}
-                inputProps={{
-                  value: cliente,
-                  onChange: (e) => setCliente(e) || setFilters((prev) => ({ ...prev, cliente: e?.value })),
-                }}
-              />
-              <SearchSelect
-                label="Articolo"
-                options={searchOptions(articoli, "nome")}
-                inputProps={{
-                  onChange: (e) => setFilters((prev) => ({ ...prev, articolo_id: e?.value })),
-                }}
-              />
-              <SearchSelect
-                label="Anno"
-                options={anni.map((a) => ({ label: a, value: a }))}
-                inputProps={{
-                  onChange: (e) => setFilters((prev) => ({ ...prev, anno: e?.value })),
-                }}
-              />
-              <SearchSelect
-                label="Mese"
-                options={MESI.map((a, idx) => ({ label: a, value: idx+1 }))}
-                inputProps={{
-                  onChange: (e) => setFilters((prev) => ({ ...prev, mese: e?.value })),
-                }}
-              />
-              <Input
-                label="N° Bolla"
-                inputProps={{ onChange: (e) => setFilters((prev) => ({ ...prev, n_bolla: e.target.value })) }}
-              />
-              <Input
-                label="N° Lotto"
-                inputProps={{ onChange: (e) => setFilters((prev) => ({ ...prev, n_lotto_super: e.target.value })) }}
-              />
+            <Card.Body className="px-5 mt-3 mb-1 ">
+              <div className="grid grid-cols-2 gap-y-3 text-right">
+                <SearchSelect
+                  label="Cliente"
+                  options={searchOptions(clientiQuery.data, "nome")}
+                  inputProps={{
+                    value: cliente,
+                    onChange: (e) => setCliente(e) || setFilters((prev) => ({ ...prev, cliente: e?.value })),
+                  }}
+                />
+                <SearchSelect
+                  label="Articolo"
+                  options={searchOptions(articoli, "nome")}
+                  inputProps={{
+                    onChange: (e) => setFilters((prev) => ({ ...prev, articolo_id: e?.value })),
+                  }}
+                />
+                <SearchSelect
+                  label="Anno"
+                  options={anni.map((a) => ({ label: a, value: a }))}
+                  inputProps={{
+                    onChange: (e) => setFilters((prev) => ({ ...prev, anno: e?.value })),
+                  }}
+                />
+                <SearchSelect
+                  label="Mese"
+                  options={MESI.map((a, idx) => ({ label: a, value: idx + 1 }))}
+                  inputProps={{
+                    onChange: (e) => setFilters((prev) => ({ ...prev, mese: e?.value })),
+                  }}
+                />
+                <Input
+                  label="N° Bolla"
+                  inputProps={{ onChange: (e) => setFilters((prev) => ({ ...prev, n_bolla: e.target.value })) }}
+                />
+                <Input
+                  label="N° Lotto"
+                  inputProps={{
+                    onChange: (e) => setFilters((prev) => ({ ...prev, n_lotto_super: e.target.value })),
+                  }}
+                />
+              </div>
+              <hr className="my-4 w-1/2 mx-auto" />
+              <div>
+                <DownloadButton />
+              </div>
             </Card.Body>
           </Card>
           <Tabella />
@@ -142,18 +151,51 @@ function Tabella() {
               <td colSpan={6}>Nessun record presente</td>
             </tr>
           )}
-          {recordsQuery.isLoading && Array.from(Array(25)).map((_, idx) => (
-            <tr key={idx}>
-              <td colSpan={6}>
-                <Placeholder as="p" animation="glow">
-                  <Placeholder xs={12} className="rounded-md" />
-                </Placeholder>
-              </td>
-            </tr>
-          ))}
+          {recordsQuery.isLoading &&
+            Array.from(Array(25)).map((_, idx) => (
+              <tr key={idx}>
+                <td colSpan={6}>
+                  <Placeholder as="p" animation="glow">
+                    <Placeholder xs={12} className="rounded-md" />
+                  </Placeholder>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </Table>
       <Paginator data={recordsQuery.data} setPage={setPage} />
     </>
+  );
+}
+
+function DownloadButton() {
+  const { queryKey } = usePageContext();
+  const recordsQuery = useQuery({ queryKey: queryKey, keepPreviousData: true });
+
+  const download = async () => {
+    const zip = new JSZip();
+    const urls = recordsQuery.data.results.map((record) => record.certificato).filter((url) => url !== null);
+
+    // Fetch each PDF and add it to the zip
+    const pdfFetchPromises = urls.map(async (url, index) => {
+      const response = await fetch(url);
+      const arrayBuffer = await response.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+      zip.file(url.split("/").at(-1), buffer);
+    });
+
+    // Wait for all fetches to complete
+    await Promise.all(pdfFetchPromises);
+
+    // Generate ZIP file and trigger download
+    zip.generateAsync({ type: "nodebuffer" }).then((content) => {
+      electron.ipcRenderer.invoke("save-zip", content, "certificati.zip");
+    });
+  };
+
+  return (
+    <Button className="bg-[#0d6efd]" onClick={download}>
+      <FontAwesomeIcon icon={faDownload} className="mr-2" /> Download Certificati
+    </Button>
   );
 }
