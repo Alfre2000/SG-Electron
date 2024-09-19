90e2a365a8959f93723487bba1dee826f5fe6f75
Alfredo <alfredo.mauri.2000@gmail.com>
2024-09-19 15:29:44 +0200

    allarmi quando manca una barra

--- src/pages/GestioneImpianto/RecordLavorazione/Allarmi.jsx
+++ src/pages/GestioneImpianto/RecordLavorazione/Allarmi.jsx
@@ -8,6 +8,7 @@ import { Alert } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowCircleRight, faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import { useImpianto } from "@contexts/UserContext";
 const { motion } = require("framer-motion");
 
 const alert = {
@@ -25,10 +26,19 @@ const alerts = {
 };
 
 function Allarmi() {
+  const impianto = useImpianto();
   const schedaImpiantoQuery = useImpiantoQuery({ queryKey: URLS.ULTIMA_SCHEDA_IMPIANTO });
   const isSchedaImpiantoOld = schedaImpiantoQuery.data?.id && !isDateRecent(schedaImpiantoQuery.data.data, 8);
 
   const richiesteQuery = useImpiantoQuery({ queryKey: [URLS.RICHIESTE_CORREZIONE_BAGNO, { eseguita: "false" }] });
+  const today = new Date().toISOString().split("T")[0];
+  const missingBarre = useImpiantoQuery(
+    { queryKey: [URLS.BARRE, { inizio: today, empty: true, finita: false }] },
+    {
+      select: (data) => data.results.filter((barra) => isDateRecent(barra.inizio, 3)),
+      refetchInterval: 1000 * 60
+    }
+  );
 
   const avvisiQuery = useImpiantoQuery(
     { queryKey: URLS.PAGINA_PROSSIME },
@@ -60,6 +70,33 @@ function Allarmi() {
       link: operazione.link,
     });
   });
+  const enabled = [2, 8].includes(impianto);
+  if (enabled && missingBarre.data?.length > 0) {
+    const single = missingBarre.data?.length === 1;
+    allarmi.push({
+      title: single ? "Barra mancante:" : "Barre mancanti:",
+      message: (
+        <span className="font-normal">
+          Inserire il codice del foglio giallo per {single ? "la barra" : "le barre"}: <br />
+          {missingBarre.data?.map((barra) => (
+            <span key={barra.id} className="font-semibold">
+              {barra.codice.split("-").at(-1)}{" "}
+              {barra.articolo ? (
+                <>
+                  {" "}
+                  <span className="mx-3">-</span>
+                  {barra.articolo}{" "}
+                </>
+              ) : null}
+              <span className="mx-3">-</span> Iniziata alle: {barra.inizio.split("T")[1].slice(0, 5)}
+              <br />
+            </span>
+          ))}
+        </span>
+      ),
+      link: "",
+    });
+  }
   return (
     <motion.div variants={alerts} initial="hidden" animate="show">
       {allarmi.map((allarme) => (
@@ -68,9 +105,11 @@ function Allarmi() {
             <FontAwesomeIcon icon={faTriangleExclamation} className="mr-10"></FontAwesomeIcon>
             <div className="w-[30%]">{allarme.title}</div>
             <b className="pl-4 w-[55%] pr-2">{allarme.message}</b>
-            <Link to={allarme.link}>
-              <FontAwesomeIcon icon={faArrowCircleRight} size="lg" />
-            </Link>
+            {allarme.link === "" ? null : (
+              <Link to={allarme.link}>
+                <FontAwesomeIcon icon={faArrowCircleRight} size="lg" />
+              </Link>
+            )}
           </Alert>
         </motion.div>
       ))}
